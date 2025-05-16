9e80a1678b6eb0707f47aee3abf4dea242d22e46
Mif-Inovations <muhammadirsyaadfadhillah@gmail.com>
2025-05-16 21:56:10 +0700

    Fix: Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
 import React from 'react'; // import library react
-import { Routes, Route, Navigate } from 'react-router-dom'; // import library react-router-dom
-import Home from './pages/Home';
-import Login from './pages/Login'; // import halaman Login
-import Register from './pages/Register'; // import halaman Register
+import { BrowserRouter } from 'react-router-dom'; // import library react-router-dom
+// Fungsi dari library react-router-dom untuk routing/pemindahan halaman
+import AppRoutes from './routes/AppRoutes'; // import AppRoutes.jsx dari folder routes
 
-// ini pakai arrow function juga
-const AppRoutes = () => {
+/* ada berbagai jenis functional components yang sering di pakai:
+- function declaration
+- arrow function
+*/
+
+// ini pakai arrow function
+const App = () => {
   return (
-    <Routes>
-      {/* routingnya ada di sini*/}
-      <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/register" element={<Register />} />
-    </Routes>
+    <BrowserRouter>
+      {/* Memanggil AppRoutes */}
+      <AppRoutes />
+    </BrowserRouter>
   );
 };
 
-// ekspor AppRoutes
-export default AppRoutes;
+// ini untuk ekspor functionnya
+export default App;
--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["framer-motion"],
-    },
-  },
-  resolve: {
-    alias: {
-      "framer-motion": "node_modules/framer-motion",
-    },
-  },
-});
+  plugins: [react()], 
+})
